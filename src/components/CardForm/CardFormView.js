8d94ea9c14f96f173885185fff6c1be13e97f880
Kaitlyn Hova <kaitlynhova@gmail.com>
2018-07-02 22:15:20 -0700

    Connected form submit to firebase

--- src/components/CardForm/CardFormView.js
+++ src/components/CardForm/CardFormView.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
 const formFields = ['reason', 'name', 'email', 'location'];
 const CardFormView = props => (
   <div>
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        // TODO, instead create cardForm prop to hold the cardform state
+        const formBodyObject = {};
+        formFields.forEach((formField) => {
+          formBodyObject[formField] = props[formField];
+        });
+        formBodyObject.id = v4();
+
+        fetch(`https://supportcards-bb694.firebaseio.com/cards/${formBodyObject.id}.json`, {
+          method: 'PUT',
+          body: JSON.stringify(formBodyObject),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }}
+    >
       {formFields.map(field => (
         <input
           type="text"
@@ -15,12 +34,9 @@ const CardFormView = props => (
           value={props[field]}
         />
       ))}
+      <input type="submit" name="submit" value="Submit" />
     </form>
   </div>
 );
-CardFormView.propTypes = {
-  // eslint-disable-next-line
-  updateFormField: PropTypes.func.isRequired,
-};
 
 export { CardFormView };
